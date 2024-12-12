f3b1664182842c87c8f51aad10677b5a93eb5436
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-12 15:32:28 +0530

    add logic to fetch the version

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -290,7 +290,7 @@ parameters {
                 
                 PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
                 echo "PS_RELEASE : ${PS_RELEASE}"
-                PS_VERSION_KEY= PS_RELEASE.execute("awk -F'.' '{print \$1 \".\" \$2}'").text.trim()
+                PS_VERSION_KEY= PS_RELEASE..execute(["awk", "-F.", "{print \$1 \".\" \$2}"]).text.trim()
                 echo "Version is for : ${PS_VERSION_KEY}"
 
                 def KEY_VER
