7c39081af8738f15a68dcc80a9b00b66db65ae45
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-13 13:45:26 +0530

    debug to add logic to fetch the version

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -309,13 +309,19 @@ parameters {
                         git config user.email "it+jenkins-pxc-cd@percona.com"
                         git checkout testing-branch 
                         echo "${KEY_VER} is the VALUE!!@!"
-                        export RELEASE_VER_VAL="${KEY_VER}"
+                        export RELEASE_VER_VAL="${KEY_VER}_VER"
                         if [[ "\$RELEASE_VER_VAL" =~ ^8.[0-9]{1}\$ ]]; then
                             echo "\$RELEASE_VER_VAL is a valid version"
                             OLD_REV=\$(cat VERSIONS | grep ${KEY_VER}_REV | cut -d '=' -f2- )
                             echo "OLD_REV is : \${OLD_REV}"
                             OLD_VER=\$(cat VERSIONS | grep ${KEY_VER}_VER | cut -d '=' -f2- )
                             echo "OLD_VER is : \${OLD_VER}"
+                            echo "OLD_REV=\$OLD_REV" > old_rev.txt
+                            echo "OLD_VER=\$OLD_VER" > old_ver.txt
+                            def OLD_REV = readFile('old_rev.txt').trim()
+                            def OLD_VER = readFile('old_ver.txt').trim()
+                            echo "Captured OLD_REV: ${OLD_REV}"
+                            echo "Captured OLD_VER: ${OLD_VER}"
                             sed -i s/PS_INN_LTS_REV=\$OLD_REV/${KEY_VER}_REV='"'${REVISION}'"'/g VERSIONS
                             sed -i s/PS_INN_LTS_VER=\$OLD_VER/${KEY_VER}_VER='"'${PS_RELEASE}'"'/g VERSIONS
 
