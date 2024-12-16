527048fb622f322f5a86bb1d9d7e2752409fb720
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-16 17:35:34 +0530

    adding changes to declare variable globally

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -139,17 +139,17 @@ def runPlaybook(def nodeName) {
         unstash 'properties' 
         script {
                // currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
-                env.REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
-                sh "cat test/percona-server-8.0.properties"
-                
-                env.PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
-                echo "PS_RELEASE : ${env.PS_RELEASE}"
-                env.PS_VERSION_KEY=  sh(script: """echo ${PS_RELEASE} | awk -F'.' '{print \$1 \".\" \$2}'""", returnStdout: true).trim()
-                echo "Version is for : ${env.PS_VERSION_KEY}"
-                env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
-                echo "Value is : ${env.KEY_VER}"
-               // PS8_RELEASE_VERSION = sh(returnStdout: true, script: """ echo ${BRANCH} | sed -nE '/release-(8\\.[0-9]{1})\\..*/s//\\1/p' """).trim()
+            env.REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
+            sh "cat test/percona-server-8.0.properties"
+            env.PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
+            echo "PS_RELEASE : ${env.PS_RELEASE}"
+            env.PS_VERSION_KEY=  sh(script: """echo ${PS_RELEASE} | awk -F'.' '{print \$1 \".\" \$2}'""", returnStdout: true).trim()
+            echo "Version is for : ${env.PS_VERSION_KEY}"
+            env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
+            echo "Value is : ${env.KEY_VER}"
+            // PS8_RELEASE_VERSION = sh(returnStdout: true, script: """ echo ${BRANCH} | sed -nE '/release-(8\\.[0-9]{1})\\..*/s//\\1/p' """).trim()
         }
+
         def playbook //= "ps_80.yml"
         def playbook_path //= "package-testing/playbooks/${playbook}"
 
@@ -235,12 +235,12 @@ pipeline {
     agent {
         label 'docker'
     }
-    /*environment {
+    environment {
         REVISION = ""
         PS_RELEASE = ""
         PS_VERSION_KEY = ""
         KEY_VER = ""
-    }*/
+    }
 
 parameters {
         string(defaultValue: 'https://github.com/percona/percona-server.git', description: 'github repository for build', name: 'GIT_REPO')
@@ -283,7 +283,17 @@ parameters {
         timestamps ()
     }
     stages {
-
+        stage('Preparation') {
+            steps {
+                script {
+                    runPlaybook("nodeName")
+                    echo "Revision: ${env.REVISION}"
+                    echo "PS_RELEASE: ${env.PS_RELEASE}"
+                    echo "PS_VERSION_KEY: ${env.PS_VERSION_KEY}"
+                    echo "KEY_VER: ${env.KEY_VER}"
+                }
+            }
+        }
         stage('Create PS source tarball') {
             agent {
                label 'min-focal-x64'
