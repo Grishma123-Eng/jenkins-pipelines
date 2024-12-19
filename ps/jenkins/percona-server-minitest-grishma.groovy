3b805758c6f31e0096d36532a1b807cbbd956364
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-19 13:17:00 +0530

    debugging

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -293,14 +293,18 @@ parameters {
                     echo "PS_VERSION_KEY: ${env.PS_VERSION_KEY}"
                     env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
                     echo "KEY_VER: ${env.KEY_VER}"
+
                     if (env.KEY_VER == 'PS80' ) {
                         env.product_to_test = "PS80"
+                        echo "product to test is ps80"
                     } 
                     else if (env.KEY_VER == 'PS84' ) {
                         env.product_to_test = "PS84"
+                        echo "product to test is ps84"
                     } 
                     else {
                         env.product_to_test = 'client_test'
+                        echo "product to test is client_test"
                     }
                     echo "Product to test: ${env.product_to_test}"
                     }
