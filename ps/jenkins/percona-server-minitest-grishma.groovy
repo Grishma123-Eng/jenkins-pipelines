76aa3ebc2d6391353dd8b23b72874395ac733962
Grishma123-Eng <grishmashah121@gmail.com>
2024-12-19 12:49:24 +0530

    adding variable globally

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -132,27 +132,9 @@ def installDependencies(def nodeName) {
     }
 
 }
-
-def setKeyVer() {
-    unstash 'properties' 
-        script {
-               // currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
-            env.REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
-            sh "cat test/percona-server-8.0.properties"
-            env.PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
-            echo "PS_RELEASE : ${env.PS_RELEASE}"
-            env.PS_VERSION_KEY=  sh(script: """echo ${PS_RELEASE} | awk -F'.' '{print \$1 \".\" \$2}'""", returnStdout: true).trim()
-            echo "Version is for : ${env.PS_VERSION_KEY}"
-            env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
-            echo "Value is : ${env.KEY_VER}"
-            // PS8_RELEASE_VERSION = sh(returnStdout: true, script: """ echo ${BRANCH} | sed -nE '/release-(8\\.[0-9]{1})\\..*/s//\\1/p' """).trim()
-        }
-}
-
 def runPlaybook(def nodeName) {
-
     try {
-        /*unstash 'properties' 
+        unstash 'properties' 
         script {
                // currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
             env.REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
@@ -163,7 +145,7 @@ def runPlaybook(def nodeName) {
             echo "Version is for : ${env.PS_VERSION_KEY}"
             env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
             echo "Value is : ${env.KEY_VER}"
-        } */
+        } 
         echo "Using KEY_VER in another function: ${env.KEY_VER}"
         def playbook //= "ps_80.yml"
         def playbook_path //= "package-testing/playbooks/${playbook}"
@@ -204,19 +186,6 @@ def runPlaybook(def nodeName) {
     }
 }
 
-def Fetch_Product_to_test() {
-    script{
-    echo "Using KEY_VER in another function: ${env.KEY_VER}"
-    if (env.KEY_VER == 'PS80' || env.KEY_VER == 'PS84') {
-        env.product_to_test = "${env.KEY_VER}"
-        echo "Valid PS version: ${env.KEY_VER}"
-    } else {
-        env.product_to_test = "client_test"
-        echo "PS version is client_test"
-    }
-    }
-}
-
 def minitestNodes = [  "min-bullseye-x64",
                        "min-bookworm-x64",
                        //"min-centos-7-x64",
@@ -246,7 +215,7 @@ def package_tests_ps80(def nodes) {
 
 @Field def mini_test_error = "False"
 def AWS_STASH_PATH
-/*def product_to_test
+def product_to_test
 if (env.KEY_VER == 'PS80' || env.KEY_VER == 'PS84') {
 product_to_test = "${env.KEY_VER}"
 }
@@ -267,10 +236,10 @@ pipeline {
         label 'docker'
     }
     environment {
-        REVISION = ""
+       /* REVISION = ""
         PS_RELEASE = ""
         PS_VERSION_KEY = ""
-        KEY_VER = ""
+        KEY_VER = "" */
         product_to_test = ""
     }
 
@@ -315,39 +284,28 @@ parameters {
         timestamps ()
     }
     stages {
-        /*stage('Preparation') {
+        stage('Preparation') {
             steps {
                 script {
-                    def playbookValues = runPlaybook("nodeName")
-                    if (playbookValues != null) {
-                        env.REVISION = playbookValues.REVISION
-                        env.PS_RELEASE = playbookValues.PS_RELEASE
-                        env.PS_VERSION_KEY = playbookValues.PS_VERSION_KEY
-                        env.KEY_VER = playbookValues.KEY_VER
+                    env.PS_RELEASE = sh(script: "echo ${BRANCH} | sed 's/release-//g'", returnStdout: true).trim()
+                    echo "PS_RELEASE: ${env.PS_RELEASE}"
+                    env.PS_VERSION_KEY = "${env.PS_RELEASE}".split('\\.')[0..1].join('.')
+                    echo "PS_VERSION_KEY: ${env.PS_VERSION_KEY}"
+                    env.KEY_VER = "PS${env.PS_VERSION_KEY.replace('.', '')}"
+                    echo "KEY_VER: ${env.KEY_VER}"
+                    if (env.KEY_VER == 'PS80' || env.KEY_VER == 'PS84') {
+                        env.product_to_test = env.KEY_VER
                     } else {
-                        echo "Error: runPlaybook did not return valid values."
+                        env.product_to_test = 'client_test'
+                    }
+                    echo "Product to test: ${env.product_to_test}"
                     }
                 }
             }
-        }*/
-        stage('Prepare') {
+        stage('Run Playbook') {
             steps {
                 script {
-                    setKeyVer()
-                }
-            }
-        }
-        stage('USE Variable') {
-            steps {
-                script {
-                    runPlaybook()
-                }
-            }
-        }
-        stage('Fetch_product_to_test') {
-            steps {
-                script {
-                    Fetch_Product_to_test()
+                    echo "Product to test in playbook: ${env.product_to_test}"
                 }
             }
         }
@@ -403,7 +361,15 @@ parameters {
                 echo "Version is for : ${PS_VERSION_KEY}"
                 KEY_VER = "PS${PS_VERSION_KEY.replace('.', '')}"
                 echo "Value is : ${KEY_VER}"
-
+                
+                if (env.product_to_test == 'PS80') {
+                    echo "Running PS80-specific steps"
+                } else if (env.product_to_test == 'PS84') {
+                    echo "Running PS84-specific steps"
+                } else {
+                    echo "Running client test"
+                }
+                
             // PS8_RELEASE_VERSION = sh(returnStdout: true, script: """ echo ${BRANCH} | sed -nE '/release-(8\\.[0-9]{1})\\..*/s//\\1/p' """).trim()
                 if("${KEY_VER}"){
                     echo "Executing MINITESTS as VALID VALUES FOR PS8_RELEASE_VERSION:${KEY_VER}"
