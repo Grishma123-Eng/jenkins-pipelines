cfd6f2fc71210b746a5ee44d380816a3ff9a2ac7
Grishma123-Eng <grishmashah121@gmail.com>
2025-01-01 12:59:22 +0530

    modifying the revision value

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -367,14 +367,14 @@ parameters {
             unstash 'properties' 
             script {
                // currentBuild.description = "Built on ${BRANCH}; path to packages: ${COMPONENT}/${AWS_STASH_PATH}"
-                PS_REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
+                env.PS_REVISION = sh(returnStdout: true, script: "grep REVISION test/percona-server-8.0.properties | awk -F '=' '{ print\$2 }'").trim()
                 sh "cat test/percona-server-8.0.properties"
                 /*PS_RELEASE = sh(returnStdout: true, script: "echo ${BRANCH} | sed 's/release-//g'").trim()
                 echo "PS_RELEASE : ${PS_RELEASE}"
                 PS_VERSION_SHORT_KEY=  sh(script: """echo ${PS_RELEASE} | awk -F'.' '{print \$1 \".\" \$2}'""", returnStdout: true).trim()
                 echo "Version is for : ${PS_VERSION_SHORT_KEY}"
                 PS_VERSION_SHORT = "PS${PS_VERSION_SHORT_KEY.replace('.', '')}" */
-                echo "Revision is: ${PS_REVISION}"
+                echo "Revision is: ${env.PS_REVISION}"
                 echo "PS_RELEASE is: ${PS_RELEASE}"
                 echo "PS_VERSION_SHORT_KEY is: ${PS_VERSION_SHORT_KEY}"
                 echo "Value is : ${PS_VERSION_SHORT}"
@@ -435,7 +435,7 @@ parameters {
                     }
 
             parallel(
-                "Trigger Package Testing Job":{
+               /* "Trigger Package Testing Job":{
                     node ( 'docker' ) {
                     script {
                         echo "TRIGGERING THE PACKAGE TESTING JOB!!!"
@@ -454,7 +454,7 @@ parameters {
                         } 
                     }
                     }
-                },
+                },*/
                 
                 "Triggering Docker for ARM64":{
                     node ( 'docker-32gb-aarch64' ){
@@ -503,7 +503,7 @@ parameters {
                             ls -l ./run.sh
                             chmod +x ./run.sh
                             export PS_VERSION="${PS_RELEASE}"
-                            echo "printing variables: \$DOCKER_ACC , \$PS_VERSION , \$PS_REVISION "
+                            echo "printing variables: \$DOCKER_ACC , \$PS_VERSION , \${PS_REVISION} "
                             ./run.sh
                             echo "ran for ARM"
                         '''
