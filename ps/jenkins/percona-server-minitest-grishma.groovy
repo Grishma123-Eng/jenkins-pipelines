3cf68982c6d9cfbb20a4f50c6f8aaa566ef9ca3f
Grishma123-Eng <grishmashah121@gmail.com>
2025-01-02 16:38:28 +0530

    remove commenting part

--- ps/jenkins/percona-server-minitest-grishma.groovy
+++ ps/jenkins/percona-server-minitest-grishma.groovy
@@ -184,14 +184,14 @@ def runPlaybook(def nodeName) {
 }
 
 def minitestNodes = [   "min-bullseye-x64",
-                        "min-bookworm-x64" ]
-                       //"min-centos-7-x64",
-                       //"min-ol-8-x64",
-                      // "min-focal-x64",
-                      // "min-amazon-2-x64",
-                      // "min-jammy-x64",
-                      // "min-noble-x64",
-                       //"min-ol-9-x64"     ]
+                        "min-bookworm-x64" 
+                        "min-centos-7-x64",
+                        "min-ol-8-x64",
+                        "min-focal-x64",
+                        "min-amazon-2-x64",
+                        "min-jammy-x64",
+                        "min-noble-x64",
+                        "min-ol-9-x64"     ]
 
 
 def package_tests_ps80(def nodes) {
