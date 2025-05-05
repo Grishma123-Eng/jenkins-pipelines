4fa2fdd20a1556124f91087d7a1fd04dd9ed020d
Grishma123-Eng <grishmashah121@gmail.com>
2025-05-05 20:16:43 +0530

    debug

--- pxc/jenkins/pxc-binary-tarball-pro.groovy
+++ pxc/jenkins/pxc-binary-tarball-pro.groovy
@@ -19,6 +19,7 @@ pipeline {
     MOLECULE_DIR = "molecule/test-pxc-binary-tarball-pro/";
     PRO = "${params.PRO}"
     PXC_VERSION = "${params.PXC_VERSION}"
+    WSREP_VERSION = "${WSREP_VERSION}"
   }
   parameters {
     string(
