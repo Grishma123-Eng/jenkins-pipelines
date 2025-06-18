1162cf20e1c953f4cba10611aa06765b96f9e1f5
Grishma123-eng <grishmashah121@gmail.com>
2025-06-18 16:26:10 +0530

    adding script for proxysql tarball

---
+++ proxysql/proxysql-tarball.groovy
@@ -0,0 +1,99 @@
+
+library changelog: false, identifier: "lib@tarball_task", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/grishma123-eng/jenkins-pipelines.git'
+])
+
+
+
+def operatingsystems() {
+    return ['ubuntu-noble','ubuntu-jammy','debian-11','debian-12','oracle-8','oracle-9']
+}
+
+
+pipeline {
+  agent {
+    label 'min-bookworm-x64'
+  }
+  environment {
+    PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+    MOLECULE_DIR = "molecule/proxysql-tarball/";
+    PROXYSQL_VERSION = "${params.PROXY_VERSION}"
+    WSREP_VERSION = "${params.WSREP_VERSION}"
+  }
+  parameters {
+    string(
+      name: 'Proxysql_version', 
+      defaultValue: '8.0.36-28', 
+      description: 'proxysql full version'
+    )
+    string(
+      name: 'REVISION', 
+      defaultValue: '47601f19', 
+      description: 'proxysql revision'
+    )
+    string(
+      name: 'WSREP_VERSION',
+      defaultValue: '26.1.4.3', 
+      description: 'WSREP version'
+      )
+    choice(
+      choices: 'testing\nmain',
+      description: 'PXC repo name',
+      name: 'REPO')
+    string(
+      defaultValue: 'master',
+      description: 'Branch for package-testing repository',
+      name: 'TESTING_BRANCH'
+    )
+    string(
+      defaultValue: 'Percona-QA',
+      description: 'Git account for package-testing repository',
+      name: 'TESTING_GIT_ACCOUNT'
+    )
+  }
+  options {
+    withCredentials(moleculepxcJenkinsCreds())
+    disableConcurrentBuilds()
+  }
+  stages {
+    stage('Set build name'){
+      steps {
+        script {
+          currentBuild.displayName = "${env.BUILD_NUMBER}-${env.Proxysql_version}"
+          currentBuild.description = "${env.REVISION}-${env.TESTING_BRANCH}-${env.TESTING_GIT_ACCOUNT}"
+        }
+      }
+    }
+    stage('Checkout') {
+      steps {
+        deleteDir()
+        git poll: false, branch: TESTING_BRANCH, url: "https://github.com/${TESTING_GIT_ACCOUNT}/package-testing.git"
+      }
+    }
+    stage ('Prepare') {
+      steps {
+        script {
+          installMoleculeBookworm()
+        }
+      }
+    }
+
+    stage('Run tarball molecule') {
+      steps {
+        withCredentials([usernamePassword(credentialsId: 'PS_PRIVATE_REPO_ACCESS', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+          script {
+            moleculeParallelTest(operatingsystems(), env.MOLECULE_DIR)
+          }
+        }
+      }
+    }
+  }
+  post {
+    always {
+      script {
+        moleculeParallelPostDestroy(operatingsystems(), env.MOLECULE_DIR)
+      }
+    }
+  }
+}
