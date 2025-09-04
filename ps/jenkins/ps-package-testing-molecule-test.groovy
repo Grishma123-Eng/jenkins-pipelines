70ec80a08c1cbb6c93d7c7ce4cc4cb0ab9d7cda7
Grishma123-eng <grishmashah121@gmail.com>
2025-09-04 16:07:50 +0530

    testing required changes

---
+++ ps/jenkins/ps-package-testing-molecule-test.groovy
@@ -0,0 +1,230 @@
+
+
+    library changelog: false, identifier: "lib@ps-9x", retriever: modernSCM([
+        $class: 'GitSCMSource',
+        remote: 'https://github.com/grishma123-eng/jenkins-pipelines.git'
+    ])
+
+    pipeline {
+    agent {
+        label 'min-bookworm-x64'
+    }
+    environment {
+        product_to_test = "${params.product_to_test}"
+        git_repo = "${params.git_repo}"
+        install_repo = "${params.install_repo}"
+        action_to_test  = "${params.action_to_test}"
+        check_warnings = "${params.check_warnings}"
+        install_mysql_shell = "${params.install_mysql_shell}"
+    }
+    parameters {
+        choice(
+            choices: ['ps_80', 'ps_84', 'ps_lts_innovation','client_test'],
+            description: 'Choose the product version to test: PS8.0 OR ps_lts_innovation',
+            name: 'product_to_test'
+        )
+        choice(
+            choices: ['testing', 'main', 'experimental'],
+            description: 'Choose the repo to install packages and run the tests',
+            name: 'install_repo'
+        )
+        string(
+            defaultValue: 'https://github.com/Percona-QA/package-testing.git',
+            description: 'repo name',
+            name: 'git_repo',
+            trim: false
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Branch name',
+            name: 'git_branch',
+            trim: false
+        )
+        choice(
+            choices: [
+                'install',
+                'upgrade',
+                'major_upgrade_to',
+                'kmip',
+                'kms'
+            ],
+            description: 'Action To Test',
+            name: 'action_to_test'
+        )
+        choice(
+            choices: [
+                'yes',
+                'no',
+            ],
+            description: 'check_warnings',
+            name: 'check_warnings'
+        )
+        choice(
+            choices: [
+                'yes',
+                'no'
+            ],
+            description: 'Install MySQL Shell',
+            name: 'install_mysql_shell'
+        )
+    }
+    options {
+        withCredentials(moleculePdpsJenkinsCreds())
+    }
+
+        stages {
+            stage('Set Build Name'){
+                steps {
+                    script {
+                        currentBuild.displayName = "${env.BUILD_NUMBER}-${product_to_test}-${action_to_test}"
+                    }
+                }
+            }
+
+            stage('Checkout') {
+                steps {
+                    deleteDir()
+                    git poll: false, branch: "${params.git_branch}", url: "${params.git_repo}"      }
+            }
+
+            stage('Prepare') {
+                steps {
+                    script {
+                        installMolecule()
+                    }
+                }
+            }
+            stage('RUN TESTS') {
+                        steps {
+                            script {
+                                if (action_to_test == 'install') {
+                                    sh """
+                                        echo PLAYBOOK_VAR="${product_to_test}" > .env.ENV_VARS
+                                    """
+                                } else {
+                                    sh """
+                                        echo PLAYBOOK_VAR="${product_to_test}_${action_to_test}" > .env.ENV_VARS
+                                    """
+                                }
+
+                                 def envMap = loadEnvFile('.env.ENV_VARS')
+                                withEnv(envMap) {
+                                    if (product_to_test == "ps_lts_innovation") {
+                                        moleculeParallelTestPS(ps90PackageTesting(), "molecule/ps/")
+                                }   else {
+                                            moleculeParallelTestPS(psPackageTesting(), "molecule/ps/")
+                                        }
+                                    }
+                            }
+                        }
+            }
+        }
+
+    post {
+        always {
+            deleteBuildInstances()
+        }
+    }
+    }
+
+def deleteBuildInstances(){
+    script {
+        echo "All tests completed"
+
+        def awsCredentials = [
+                sshUserPrivateKey(
+                    credentialsId: 'MOLECULE_AWS_PRIVATE_KEY',
+                    keyFileVariable: 'MOLECULE_AWS_PRIVATE_KEY',
+                    passphraseVariable: '',
+                    usernameVariable: ''
+                ),
+                aws(
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    credentialsId: '5d78d9c7-2188-4b16-8e31-4d5782c6ceaa',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                )
+        ]
+
+        withCredentials(awsCredentials) {
+            def jobName = env.JOB_NAME
+            def BUILD_NUMBER = env.BUILD_NUMBER
+            jobName.trim()
+
+            echo "Fetched JOB_TO_RUN from environment: '${jobName}'"
+
+            echo "Listing EC2 instances with job-name tag: ${jobName}"
+            sh """
+            aws ec2 describe-instances --region us-west-2 --filters "Name=tag:job-name,Values=${jobName}" "Name=tag:build-number,Values=${BUILD_NUMBER}"  --query "Reservations[].Instances[].InstanceId" --output text
+            """
+
+            sh """
+            echo "=== EC2 Instances to be cleaned up ==="
+            aws ec2 describe-instances --region us-west-2 \\
+            --filters "Name=tag:job-name,Values=${jobName}" "Name=tag:build-number,Values=${BUILD_NUMBER}" \\
+            --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0],State.Name]" \\
+            --output table || echo "No instances found with job-name tag: ${jobName}"
+            """
+
+            def instanceIds = sh(
+                script: """
+                aws ec2 describe-instances --region us-west-2 \\
+                --filters "Name=tag:job-name,Values=${jobName}" "Name=tag:build-number,Values=${BUILD_NUMBER}" "Name=instance-state-name,Values=running" \\
+                --query "Reservations[].Instances[].InstanceId" \\
+                --output text
+                """,
+                returnStdout: true
+            ).trim()
+
+            if (instanceIds != null && !instanceIds.trim().isEmpty()) {
+                echo "Found instances to terminate: ${instanceIds.trim()}"
+
+                sh """
+                echo "${instanceIds.trim()}" | xargs -r aws ec2 terminate-instances --instance-ids
+                """
+            
+                sleep(30)
+                
+                echo "Terminated instances: ${instanceIds.trim()}"
+                
+                echo "==========================================="
+
+                echo "Verification: Status of terminated instances:"
+                
+                sh """
+                sleep 5 && aws ec2 describe-instances --instance-ids ${instanceIds} --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0],State.Name]" --output table
+                """            
+            
+            } else {
+                echo "No instances found to terminate"
+            }
+        }
+    }
+}
+
+def installMolecule() {
+        sh """
+            sudo apt update -y
+            sudo apt install -y python3 python3-pip python3-dev python3-venv
+            python3 -m venv virtenv
+            . virtenv/bin/activate
+            python3 --version
+            python3 -m pip install --upgrade pip
+            python3 -m pip install --upgrade setuptools
+            python3 -m pip install --upgrade setuptools-rust
+            python3 -m pip install --upgrade PyYaml==5.3.1 molecule==3.3.0 testinfra pytest molecule-ec2==0.3 molecule[ansible] "ansible<10.0.0" "ansible-lint>=5.1.1,<6.0.0" boto3 boto
+        """
+}
+
+def loadEnvFile(envFilePath) {
+    def envMap = []
+    def envFileContent = readFile(file: envFilePath).trim().split('\n')
+    envFileContent.each { line ->
+        if (line && !line.startsWith('#')) {
+            def parts = line.split('=')
+            if (parts.length == 2) {
+                envMap << "${parts[0].trim()}=${parts[1].trim()}"
+            }
+        }
+    }
+    return envMap
+}
