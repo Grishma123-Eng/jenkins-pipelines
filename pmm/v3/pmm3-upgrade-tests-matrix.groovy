2a6e113cea8d4ae41c60ad7c1bb674e1280e28c0
Peter Sirotnak <sirotnak.p@gmail.com>
2025-10-07 14:34:26 +0200

    PMM-7: New PMM Version 3.4.1

--- pmm/v3/pmm3-upgrade-tests-matrix.groovy
+++ pmm/v3/pmm3-upgrade-tests-matrix.groovy
@@ -4,7 +4,7 @@ library changelog: false, identifier: 'lib@PMM-7-add-pmm-version-340', retriever
 ]) _
 
 def pmmVersions = pmmVersion('v3')
-def latestVersion = pmmVersions[0]
+def latestVersion = pmmVersions.last()
 
 void runUpgradeJob(String PMM_UI_GIT_BRANCH, DOCKER_TAG, DOCKER_TAG_UPGRADE, CLIENT_VERSION, CLIENT_REPOSITORY, PMM_SERVER_LATEST, PMM_QA_GIT_BRANCH, QA_INTEGRATION_GIT_BRANCH) {
     upgradeJob = build job: 'pmm3-upgrade-tests', parameters: [
